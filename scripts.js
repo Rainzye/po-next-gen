25c06a8441bcca75568f865b90b498adf99a3c39
Rainzye <Rainzye@users.noreply.github.com>
2016-10-11 16:07:08 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/ This is the official Pokemon Online Scripts
+// This is the official Pokemon Online Scripts
 // These scripts will only work on 2.0.00 or newer
 /*jshint laxbreak:true,shadow:true,undef:true,evil:true,trailing:true,proto:true,withstmt:true*/
 // You may change these variables as long as you keep the same type
