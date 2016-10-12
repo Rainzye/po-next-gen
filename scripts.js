0ad5e21348e86125961909eb3319468050591da6
Xotically <anonware@cyberservices.com>
2016-10-12 13:47:23 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ var Config = {
     bot: "Dragonite",
     kickbot: "Blaziken",
     capsbot: "Exploud",
-    channelbot: "Chatot",
+    channelbot: "Blastoise",
     checkbot: "Safari",
     coinbot: "Meowth",
     countbot: "Safari",
