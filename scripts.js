4dd24b971d3018fc1c601a58034a10698c1cccbc
Xotically <anonware@cyberservices.com>
2016-10-16 12:40:27 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -2156,7 +2156,7 @@ beforeChatMessage: function(src, message, chan) {
 
     // Auth tags 
     if (sys.auth(src) == "3") {
-        sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b><font color='blue'><span style='color: red'>(online)</span>" : "<span style='color: red'>(Offline)</span></font> " + sys.name(src) + "</b></span> » " + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+        sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b><font color='blue'>(√)</font> " + sys.name(src) + "</b></span> » " + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
         this.afterChatMessage(src, message, channel);
     } else if (sys.auth(src) == "2") {
