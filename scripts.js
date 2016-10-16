c51f8aee4300c0c22b30079f3ef9d570671bb872
Xotically <anonware@cyberservices.com>
2016-10-16 11:51:23 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ var Config = {
     bot: "Dragonite",
     kickbot: "Blaziken",
     capsbot: "Exploud",
-    channelbot: "Welcome to",
+    channelbot: ">",
     checkbot: "Safari",
     coinbot: "Meowth",
     countbot: "Safari",
@@ -2156,7 +2156,7 @@ beforeChatMessage: function(src, message, chan) {
 
     // Auth tags 
     if (sys.auth(src) == "3") {
-        sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b><font color='blue'>(Online)</font> " + sys.name(src) + "</b></span> » " + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+        sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b><font color='blue'><span style='color: red'>(online)</span>" : "<span style='color: red'>(Offline)</span></font> " + sys.name(src) + "</b></span> » " + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
         this.afterChatMessage(src, message, channel);
     } else if (sys.auth(src) == "2") {
