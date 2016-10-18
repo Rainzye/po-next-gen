ef35457a0c7d3384766cacde73c3826efe8cea7c
Xotically <anonware@cyberservices.com>
2016-10-19 00:41:33 +0100

    Update

--- scripts.js
+++ scripts.js
@@ -1360,23 +1360,23 @@ afterLogIn : function(src) {
     }
     countbot.sendMessage(src, (typeof(this.startUpTime()) == "string" ?  "Uptime: " + this.startUpTime() + ".  " : "")  + "Players online at once was: " + sys.getVal("MaxPlayersOnline") + ".");
     sys.sendHtmlMessage(src, "");
-    sys.sendHtmlMessage(src, "<span style='color: " + sys.getColor(src) + "'></p></tr><font size=4 font color=#00007f><hr><center> <img src='pokemon:num=003&gen=6' height=10> Safari Test <img src='pokemon:num=006&gen=6' height=10><i></i><br/><img src='pokemon:num=384&gen=2' height=10></center><hr><br><font color=blue>", 0);
+    sys.sendHtmlMessage(src, "<span style='color: " + sys.getColor(src) + "'></p></tr><font size=4 font color=#00007f><hr><center><i></i><br/><img src='pokemon:num=384&gen=2' height=10></center><hr><br><font color=blue>", 0);
     if (sys.name(src) == "TerminalHydreigon") {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=541&gen=6' height=10>", 0);
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black>", 0);
     } else if (sys.auth(src) == 1) {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#00b300>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=481&gen=6' height=10>", 0);
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#00b300>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=red>", 0);
     } else if (sys.auth(src) == 2) {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=301&gen=6' height=10>", 0);
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black>", 0);
     } else if (sys.auth(src) == 3) {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#002db3>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=321&gen=6' height=10>", 0);
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#002db3>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black>", 0);
     } else if (sys.auth(src) == 4) {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=491&gen=6' height=10>", 0);
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=black>", 0);
     } else if (!sys.dbRegistered(sys.name(src))) {		
-     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=blue> Joined the channel! height=10><img src='pokemon:num=223&gen=6' height=10> ", 0);
+     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=blue>", 0);
     }  else {
-        sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=blue> Joined the channel! height=10><img src='pokemon:num=381&gen=6' height=10>", 0);
+        sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + "</span><font size=3 font color=blue>", 0);
     } 
-    sys.sendMessage(src, "");
+    sys.sendMessage(src, "Test");
 
     callplugins("afterLogIn", src);
 
@@ -1960,19 +1960,19 @@ beforeChatMessage: function(src, message, chan) {
         }
     }
     
-    if (sys.auth(src) == 1) {
+    if (sys.auth(src) == 1) { //mod
 	    sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#00b300>(🌐)</font>  " + sys.name(src) + ": </b></span><font size=3 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
 	    this.afterChatMessage(src, message, channel);
-    } else if (sys.auth(src) == 2) {
+    } else if (sys.auth(src) == 2) { //Admnin
 	    sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>(🌐)</font> " + sys.name(src) + ": </b></span><font size=3 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
         this.afterChatMessage(src, message, channel);		
-    } else if (sys.auth(src) == 3) {
+    } else if (sys.auth(src) == 3) { //owner
 	    sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#002db3>(🌐)</font> " + sys.name(src) + ": </b></span><font size=3 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
 	    sys.stopEvent();
 	    this.afterChatMessage(src, message, channel);
-    } else if (sys.auth(src) == 4) { //Hiddenauth should be hidden
+    } else if (sys.auth(src) == 4) { //Hiddenauth
 	    sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500>(🌐)</font> " + sys.name(src) + ": </b></span><font size=3 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
 	    this.afterChatMessage(src, message, channel);
