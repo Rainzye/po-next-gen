4debf3108f7c0c3a2e4e36e07253cde40e629958
Xotically <anonware@cyberservices.com>
2016-10-18 14:54:44 +0100

    Updated.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 /*jshint laxbreak:true,shadow:true,undef:true,evil:true,trailing:true,proto:true,withstmt:true*/
 // You may change these variables as long as you keep the same type
 var Config = {
-    base_url: "https://raw.githubusercontent.com/Server-Scripts/po-server-goodies/master/scripts.js",
+    base_url: "https://raw.githubusercontent.com/Rainzye/po-next-gen/master/scripts.js",
     dataDir: "scriptdata/",
     bot: "Bot",
     kickbot: "Kickbot",
