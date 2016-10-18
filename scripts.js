4bb0beb0145d7c515cbad357e74fa661baf23169
Xotically <anonware@cyberservices.com>
2016-10-18 15:10:02 +0100

    Adjustments

--- scripts.js
+++ scripts.js
@@ -1361,8 +1361,8 @@ afterLogIn : function(src) {
     countbot.sendMessage(src, (typeof(this.startUpTime()) == "string" ?  "Uptime: " + this.startUpTime() + ".  " : "")  + "Players Online At Once Was: " + sys.getVal("MaxPlayersOnline") + ".");
     sys.sendHtmlMessage(src, "");
     sys.sendHtmlMessage(src, "<span style='color: " + sys.getColor(src) + "'></p></tr><font size=4 font color=#00007f><hr><center> <img src='pokemon:num=003&gen=6' height=10> Safari Test <img src='pokemon:num=006&gen=6' height=10><i></i><br/><img src='pokemon:num=384&gen=2' height=10></center><hr><br><font color=blue>", 0);
-    if (sys.name(src) == "TerminalHydreigon") {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>[Administrator]</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=635&gen=6' height=10>", 0);
+    if (sys.name(src) == "Dreko") {
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#cc0000>[Administrator]</font> " + sys.name(src) + "</span><font size=3 font color=orange> Joined the channel! height=10><img src='pokemon:num=717&gen=6' height=10>", 0);
     } else if (sys.auth(src) == 1) {
     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#00b300>[Moderator]</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel! height=10><img src='pokemon:num=488&gen=6' height=10>", 0);
     } else if (sys.auth(src) == 2) {
