fac9e4948e183e48fc6a325ed4f1245d49063825
Xotically <anonware@cyberservices.com>
2016-10-19 14:41:55 +0100

    G

--- scripts.js
+++ scripts.js
@@ -1358,7 +1358,7 @@ afterLogIn : function(src) {
     if (maxPlayersOnline > sys.getVal("MaxPlayersOnline")) {
         sys.saveVal("MaxPlayersOnline", maxPlayersOnline);
     }
-    countbot.sendMessage(src, (typeof(this.startUpTime()) == "string" ?  "<font size=3 font color=red> Uptime: " + this.startUpTime() + ".  " : "")  + "Players Online At Once Was: " + sys.getVal("MaxPlayersOnline") + ".");
+    countbot.sendHtmlMessage(src, (typeof(this.startUpTime()) == "string" ?  "<font size=3 font color=red> Uptime: " + this.startUpTime() + ".  " : "")  + "Players Online At Once Was: " + sys.getVal("MaxPlayersOnline") + ".");
     sys.sendHtmlMessage(src, "<font size=3 font color=blue> (PA)/start bulbasaur /start charmander</span> or /start squirtle We recommend. You finish /tutorial however you may wish to /skiptutorial confirm for additional guidelines about contests type /info you can view are shop to buy pokemon balls. Using /buy like /buy bait:1 anyway. Here's extra help commands /commands safari /help safari rules /commands to see the commands good luck have fun");
     sys.sendHtmlMessage(src, "<span style='color: " + sys.getColor(src) + "'></p></tr><font size=4 font color=#00007f><hr><center><i></i><br/></center><hr><br><font color=blue>", 0);
     if (sys.name(src) == "TerminalHydreigon") {
