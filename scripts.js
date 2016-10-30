7f185ce4c873ac0c4f5cf6f9981d9af29b5aca09
Xotically <anonware@cyberservices.com>
2016-10-30 11:53:37 +0000

    Updated

--- scripts.js
+++ scripts.js
@@ -1397,7 +1397,7 @@ afterLogIn : function(src) {
     } 
     sys.sendHtmlAll(src, "<font size=3 font color=purple> Website: http://safari-zone.boards.net/# <img src='item:10'> ");
    
-    sys.sendMessage(src, "", channel);
+    sys.sendMessage(src, "<font size=3 font color=purple> Website: http://safari-zone.boards.net/# <img src='item:10'> ", channel);
 
     callplugins("afterLogIn", src);
 
@@ -1998,11 +1998,11 @@ beforeChatMessage: function(src, message, chan) {
         sys.stopEvent();
 	    this.afterChatMessage(src, message, channel);
     } else if (!sys.dbRegistered(sys.name(src))) {		
-     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#0000ff><img src='item:7'></font> " + sys.name(src) + ": </b></span><font size=2 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#0000ff><img src='item:8'></font> " + sys.name(src) + ": </b></span><font size=2 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
         this.afterChatMessage(src, message, channel);
     } else { //User
- 	    sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#0000ff><img src='item:7'></font> " + sys.name(src) + ": </b></span><font size=2 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+ 	    sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#0000ff><img src='item:8'></font> " + sys.name(src) + ": </b></span><font size=2 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
 	    sys.stopEvent();
 	    this.afterChatMessage(src, message, channel);
 	    return;
